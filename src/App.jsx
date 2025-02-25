13679962294dd0cd982078a765fcb148cbbe3389
GaryChettiar <101002917+GaryChettiar@users.noreply.github.com>
2025-02-25 10:20:21 +0530

    try

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import Home from './components/home/home';
 import Network from './components/network/network';
+import Navbar from './components/navbar/navbar';
 
 function App(){
     return(
         <div>
+            <Navbar/>
+            <Home/>
             <Network/>
 
         </div>
